935544534c8fab91e76dabffd578ddc6b443473c
MSI\Maxime <megamanjpn@gmail.com>
2018-06-12 16:41:43 +0200

    Build 12.06

---
+++ Assets/Menu/WhoWon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhoWon : MonoBehaviour {
+    public int WinnerNumber;
+    public static WhoWon Singleton;
+    private void Awake()
+    {
+        if(Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Singleton = this;
+        }
+    }
+    
+}
